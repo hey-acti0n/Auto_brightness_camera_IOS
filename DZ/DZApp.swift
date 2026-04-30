47141854ada072272959d37a5ac4462a5c0a2217
action <alexemel2@yandex.ru>
2026-04-30 14:15:02 +0300

    Initial Commit

---
+++ DZ/DZApp.swift
@@ -0,0 +1,17 @@
+//
+//  DZApp.swift
+//  DZ
+//
+//  Created by Алексей Емельянов on 30.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct DZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DZTests/DZTests.swift
@@ -0,0 +1,19 @@
+//
+//  DZTests.swift
+//  DZTests
+//
+//  Created by Алексей Емельянов on 30.04.2026.
+//
+
+import Testing
+@testable import DZ
+
+struct DZTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
